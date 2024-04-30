c5cc76d36647e3d608782b3f6ebeefc2d433cb3a
Yuval Zohar <yuvalzohar36@gmail.com>
2024-04-30 23:38:08 +0300

    add Client

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from 'tailwindcss';
+
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 export default {
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     proxy: {
       "/api": {
