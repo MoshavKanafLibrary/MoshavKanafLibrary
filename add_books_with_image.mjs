d749f1b5b413d61ea0a3879d62abecaa4618b446
Amitpe2 <Amitpe2@rafael.co.il>
2024-08-07 10:05:08 +0300

    Update

--- add_books_with_image.mjs
+++ add_books_with_image.mjs
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, query, where, getDocs, updateDoc, doc, writeBatch } from 'firebase/firestore/lite';
+import { getFirestore, collection, query, where, getDocs, doc, writeBatch } from 'firebase/firestore/lite';
 import axios from 'axios';
 import cheerio from 'cheerio';
 import xlsx from 'xlsx';
@@ -15,17 +15,14 @@ const firebaseConfig = {
   measurementId: "G-P4RSY4HQZ4"
 };
 
-// אתחול Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// הגבלת מספר הבקשות המקבילות
 const limiter = new Bottleneck({
-  maxConcurrent: 15,  // מספר מקסימלי של בקשות מקבילות
-  minTime: 500      // עיכוב מינימלי בין בקשות
+  maxConcurrent: 15,
+  minTime: 500
 });
 
-// פונקציה לחיפוש תמונה ותקציר באתר סטימצקי עם ניסיון חוזר
 async function retryFetch(url, headers, retries = 3) {
   for (let i = 0; i < retries; i++) {
     try {
@@ -33,13 +30,12 @@ async function retryFetch(url, headers, retries = 3) {
       return data;
     } catch (error) {
       console.error(`Attempt ${i + 1} failed for URL: ${url}`, error);
-      if (i === retries - 1) throw error; // אם כל הניסיונות נכשלו, לזרוק את השגיאה
+      if (i === retries - 1) throw error;
     }
   }
 }
 
-// פונקציה לחיפוש תמונה ותקציר באתר סטימצקי
-async function searchImageOnSteimatzky(bookTitle) {
+async function searchImageOnSteimatzky(bookTitle, bookAuthor) {
   const searchUrl = `https://www.steimatzky.co.il/catalogsearch/result/?q=${encodeURIComponent(bookTitle)}`;
   try {
     const data = await retryFetch(searchUrl, {
@@ -47,11 +43,12 @@ async function searchImageOnSteimatzky(bookTitle) {
     });
     const $ = cheerio.load(data);
 
-    // מציאת הפריט הראשון בתוצאות החיפוש
     const firstResult = $('.product-item-image.product-image-container img').first();
     const bookLink = firstResult.closest('a').attr('href');
     let imageUrl = '';
     let summary = '';
+    let foundTitle = '';
+    let foundAuthor = '';
 
     if (firstResult.length > 0 && bookLink) {
       imageUrl = firstResult.attr('src');
@@ -59,12 +56,30 @@ async function searchImageOnSteimatzky(bookTitle) {
         imageUrl = `https://www.steimatzky.co.il${imageUrl}`;
       }
 
-      // קבלת פרטי הספר מהקישור של הספר
       const bookData = await retryFetch(bookLink, {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
       });
       const bookPage = cheerio.load(bookData);
+
+      // בדיקה בכותרת הדף
+      foundTitle = bookPage('title').text().trim();
+      const metaDescription = bookPage('meta[name="description"]').attr('content') || '';
+
+      // אם שם הספר בכותרת הדף או בתיאור המטה מתאים לשם הספר באקסל
+      if (foundTitle.includes(bookTitle) || metaDescription.includes(bookTitle)) {
+        console.log(`Found book by title in page title or meta description: ${bookTitle}`);
+        foundTitle = bookTitle;
+      } else {
+        foundTitle = ''; // איפוס המשתנה אם אין התאמה
+      }
+
       summary = bookPage('.description').text().trim();
+
+      // בדיקה נוספת אם שם הסופר נמצא בתקציר
+      if (summary.includes(bookAuthor)) {
+        console.log(`Found author in summary: ${bookAuthor}`);
+        foundAuthor = bookAuthor;
+      }
     }
 
     if (!imageUrl) {
@@ -72,36 +87,38 @@ async function searchImageOnSteimatzky(bookTitle) {
       imageUrl = 'https://preview.steimatzky.co.il/pub/static/version1721198153/frontend/Steimatzky/Theme/he_IL/Magento_Catalog/images/product/placeholder/small_image.jpg';
     }
 
-    console.log(`Found image URL: ${imageUrl} and summary for book: ${bookTitle}`);
+    console.log(`Found image URL: ${imageUrl}, summary: ${summary ? "Found" : "Not found"}, and title: ${foundTitle || "No title found"}, and author: ${foundAuthor || "No author found"} for book: ${bookTitle}`);
     return {
       imageUrl,
-      summary
+      summary,
+      foundTitle,
+      foundAuthor
     };
   } catch (error) {
     console.error(`Error fetching data from Steimatzky for book: ${bookTitle}`, error);
     return {
       imageUrl: 'https://preview.steimatzky.co.il/pub/static/version1721198153/frontend/Steimatzky/Theme/he_IL/Magento_Catalog/images/product/placeholder/small_image.jpg',
-      summary: ''
+      summary: '',
+      foundTitle: '',
+      foundAuthor: ''
     };
   }
 }
 
-// טעינת קובץ ה-Excel
-const filePath = 'C:/Program Files/Git/BOOKS_TEST.xlsx'; // נתיב לקובץ ה-Excel שלך
+const filePath = 'C:/Users/AMITPE2/Desktop/gitRepos/LibMoshavKanaf/BOOKS_TEST.xlsx';
 const workbook = xlsx.readFile(filePath);
-const sheetName = workbook.SheetNames[0]; // השתמש בשם הגיליון הראשון
+const sheetName = workbook.SheetNames[0];
 const sheet = workbook.Sheets[sheetName];
 const data = xlsx.utils.sheet_to_json(sheet);
 
-// Local caches
 let localBooksData = new Map();
 let localCopiesData = [];
 
-// פונקציה להוספת ספרים ועותקים לקאש מקומי
 async function addBooksAndCopiesToCache() {
   const promises = data.map(async (row) => {
     const bookTitle = row['שם הספר'];
     const copyID = parseInt(row['קוד ספר'], 10);
+    const bookAuthor = `${row['סופר פרטי'] || ''} ${row['סופר משפחה'] || ''}`.trim();
 
     if (!bookTitle || isNaN(copyID)) {
       console.error('Invalid data:', { bookTitle, copyID });
@@ -110,7 +127,7 @@ async function addBooksAndCopiesToCache() {
 
     let bookData = {
       title: bookTitle,
-      author: `${row['סופר פרטי'] || ''} ${row['סופר משפחה'] || ''}`.trim(),
+      author: bookAuthor,
       imageURL: row['קישור לתמונה'] || '',
       expenditure: row['הוצאה'] || '',
       titleType: row['סוג הכותרת'] || 'books',
@@ -122,9 +139,23 @@ async function addBooksAndCopiesToCache() {
     };
 
     if (!bookData.imageURL || !bookData.summary) {
-      const result = await limiter.schedule(() => searchImageOnSteimatzky(bookTitle));
-      bookData.imageURL = result.imageUrl;
-      bookData.summary = result.summary || bookData.summary;
+      const result = await limiter.schedule(() => searchImageOnSteimatzky(bookTitle, bookAuthor));
+      if ((result.foundTitle && result.foundTitle === bookTitle) || 
+          (result.foundAuthor && result.foundAuthor === bookAuthor) ||
+          (result.summary && (result.summary.includes(bookAuthor) || result.summary.includes(bookTitle)))) {
+        console.log(`Found book by title or author: ${bookTitle} or ${bookAuthor}`);
+        bookData.imageURL = result.imageUrl;
+        bookData.summary = result.summary || bookData.summary;
+        bookData.title = bookTitle;
+      } else if (result.summary && (result.summary.includes(bookTitle) || result.summary.includes(bookAuthor))) {
+        console.log(`Found book by title or author in summary: ${bookTitle} or ${bookAuthor}`);
+        bookData.imageURL = result.imageUrl;
+        bookData.summary = result.summary;
+        bookData.title = bookTitle;
+      } else {
+        console.log(`No suitable match found for book: ${bookTitle}`);
+        bookData.imageURL = 'https://preview.steimatzky.co.il/pub/static/version1721198153/frontend/Steimatzky/Theme/he_IL/Magento_Catalog/images/product/placeholder/small_image.jpg';
+      }
     }
 
     if (localBooksData.has(bookTitle)) {
@@ -149,7 +180,6 @@ async function addBooksAndCopiesToCache() {
   console.log('Books and copies added to local cache.');
 }
 
-// פונקציה לעדכון Firestore מקאש מקומי עם ניסיון חוזר
 async function retryFirestoreOperation(operation, retries = 3) {
   for (let i = 0; i < retries; i++) {
     try {
@@ -157,12 +187,11 @@ async function retryFirestoreOperation(operation, retries = 3) {
       return;
     } catch (e) {
       console.error(`Attempt ${i + 1} failed for Firestore operation`, e);
-      if (i === retries - 1) throw e; // אם כל הניסיונות נכשלו, לזרוק את השגיאה
+      if (i === retries - 1) throw e;
     }
   }
 }
 
-// פונקציה לעדכון Firestore מקאש מקומי
 async function updateFirestoreFromCache() {
   const batch = writeBatch(db);
   for (const book of localBooksData.values()) {
@@ -203,14 +232,9 @@ async function updateFirestoreFromCache() {
   console.log('Firestore updated successfully from local cache.');
 }
 
-// פונקציה עיקרית להוספת ספרים ועותקים
 async function addBooksAndCopies() {
   await addBooksAndCopiesToCache();
   await updateFirestoreFromCache();
 }
 
-// קריאת הפונקציה לה
-
-
-// קריאת הפונקציה להוספת ספרים ועותקים
 addBooksAndCopies();
