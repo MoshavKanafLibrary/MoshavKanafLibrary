5a931bb8a8377cdff9181a060edca62b72c2bc35
LAPTOP-BVA7F5Q7\amitp <amit.perets78@gmail.com>
2024-09-15 20:47:53 +0300

    Update map

--- Client/src/components/AdminLayout.js
+++ Client/src/components/AdminLayout.js
@@ -2,10 +2,16 @@ import React from 'react';
 import AdminSidebar from './AdminSidebar';
 import { Outlet } from 'react-router-dom';
 
+/**
+ * AdminLayout component that renders the main layout for the admin panel.
+ * It includes a sidebar and the main content area where the nested routes will be rendered.
+ */
 const AdminLayout = () => {
   return (
     <div className="flex min-h-screen flex-col sm:flex-row">
+      {/* AdminSidebar is the navigation bar for admin-specific options */}
       <AdminSidebar />
+      {/* Main content area where the nested components are rendered */}
       <div className="flex-1 flex flex-col bg-gray-100 p-4">
         <Outlet />
       </div>
